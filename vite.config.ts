83e2cd0850e514f5f6b9e24da5c242dc0db491ca
ChaiyoKung <kananek45@gmail.com>
2024-01-13 20:49:10 +0700

    Format code

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +7,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        app: 'popup.html'
-      }
-    }
-  }
-})
+        app: 'popup.html',
+      },
+    },
+  },
+});
